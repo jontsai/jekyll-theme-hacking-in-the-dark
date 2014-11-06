e482b2a1a033396157c68b4b15a3dafe18535f49
Jonathan Tsai <jontsai@jonathantsai.com>
2014-11-06 22:01:19 +0000

    update theme use google universal analytics with demographics tracking

--- assets/themes/hacking-in-the-dark/js/site.js
+++ assets/themes/hacking-in-the-dark/js/site.js
@@ -6,6 +6,7 @@ function (Y) {
     /* YUI "Local" Globals */
 
     // CSS selectors
+    var CSS_ID_BUTTON_STRIPE_DONATE = 'button_stripe_donate';
 
     // Nodes
     var main = Y.one('#main');
@@ -17,9 +18,28 @@ function (Y) {
 
     // Custom App Functions
 
+    function handleStripeDonateButtonPressed(e) {
+        var button = this;
+        e.preventDefault();
+        var handler = StripeCheckout.configure({
+            key: STRIPE_KEY,
+            image: GRAVATAR_IMG,
+            token: function(token, args) {
+                // Use the token to create the charge with a server-side script.
+                // You can access the token ID with `token.id`
+                Y.log('token: ' + token.id);
+            }
+        });
+        handler.open({
+            name: 'jontsai.com',
+            description: 'Donation',
+            amount: 2000
+        });
+    }
 
     // App Initializers
     function initEventHandlers() {
+        //main.delegate('tap', handleStripeDonateButtonPressed, '#' + CSS_ID_BUTTON_STRIPE_DONATE);
     }
 
     function init() {
