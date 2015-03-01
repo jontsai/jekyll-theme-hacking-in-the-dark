7dc7ca0896c398c48ee178d790d620c33d8bce3c
Jonathan Tsai <jontsai@jonathantsai.com>
2015-03-01 07:13:11 +0000

    better responsiveness

--- assets/themes/hacking-in-the-dark/js/site.js
+++ assets/themes/hacking-in-the-dark/js/site.js
@@ -1,6 +1,7 @@
 YUI().use(
     'node',
     'event',
+    'gallery-debounce',
 function (Y) {
     /* -------------------------------------------------- */
     /* YUI "Local" Globals */
@@ -8,8 +9,11 @@ function (Y) {
     // CSS selectors
     var CSS_ID_BUTTON_STRIPE_DONATE = 'button_stripe_donate';
 
+    var CSS_CLASS_BELOW_FOLD = 'below-fold';
+
     // Nodes
     var main = Y.one('#main');
+    var documentBody = Y.one(document.body);
 
     // App variables
 
@@ -17,6 +21,37 @@ function (Y) {
     /* -------------------------------------------------- */
 
     // Custom App Functions
+
+    function handleWindowResized(e) {
+        reflowContent();
+    }
+
+    function handleWindowScrolled(e) {
+        reflowContent();
+    }
+
+    function reflowContent() {
+        var scrollY = documentBody.get('docScrollY');
+        var mainY = main.getY();
+        var leftBox = Y.one('#left_box');
+        var rightBox = Y.one('#right_box');
+        if (scrollY > mainY) {
+            // get the current X, which will become the new margin
+            // left and right side are mirrored
+            var sideboxMarginLeft = leftBox.getX();
+            leftBox.addClass(CSS_CLASS_BELOW_FOLD);
+            leftBox.setStyle('marginLeft', sideboxMarginLeft);
+
+            rightBox.addClass(CSS_CLASS_BELOW_FOLD);
+            rightBox.setStyle('marginLeft', sideboxMarginLeft);
+        } else {
+            leftBox.removeClass(CSS_CLASS_BELOW_FOLD);
+            leftBox.setStyle('marginLeft', 'auto');
+
+            rightBox.removeClass(CSS_CLASS_BELOW_FOLD);
+            rightBox.setStyle('marginLeft', 'auto');
+        }
+    }
 
     function handleStripeDonateButtonPressed(e) {
         var button = this;
@@ -39,6 +74,8 @@ function (Y) {
 
     // App Initializers
     function initEventHandlers() {
+        Y.on('resize', Y.debounce(10, handleWindowResized));
+        Y.on('scroll', Y.debounce(10, handleWindowScrolled));
         //main.delegate('tap', handleStripeDonateButtonPressed, '#' + CSS_ID_BUTTON_STRIPE_DONATE);
     }
 
